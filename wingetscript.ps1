4cf11c451be445d15e2c32c86f58806b9bc9e106
Adam <ANONYM@ANONYM>
2026-09-21 19:33:27 +0200

    feat: winget can now ignore packages based on json entries

--- wingetscript.ps1
+++ wingetscript.ps1
@@ -14,29 +14,41 @@ function Get-Index {
 	)
 	if ($increment) {
 		if ($num + 1 -gt $max) { return $max }
-		else { return $num + 1 }
+  else { return $num + 1 }
 	}
 	else {
-		if ($num - 1 -lt 0) { return 0 }
+  if ($num - 1 -lt 0) { return 0 }
 		else { return $num - 1 }
 	}
 }
 
 $exit = $false
 do {
+	$cursorPos = $Host.UI.RawUI.CursorPosition
+
 	$wingetUpgradeOut = winget upgrade --include-unknown
-	
+
+	$pkgsToIgnore = @((Get-Content -Path "wingetscript.json" | ConvertFrom-Json).ignore)
+
 	$header0 = $wingetUpgradeOut[0]
 	$header1 = $wingetUpgradeOut[1]
 	$footer = $wingetUpgradeOut[$wingetUpgradeOut.Count - 1]
-	
-	$list = $wingetUpgradeOut -ne $header0 -ne $header1 -ne $footer
+
+	$list = $wingetUpgradeOut |
+	Where-Object {
+		$line = $_.Trim()
+		$line -ne "" -and
+		$line -ne $header0 -and
+		$line -ne $header1 -and
+		$line -ne $footer -and
+		-not ($pkgsToIgnore | Where-Object { $line -like "*$_*" })
+	}
 	if ($list.Count -eq 0) {
 		Write-Host "No upgradable packages parsed." -ForegroundColor Cyan
 		Invoke-ReadKey -message "Press any key to exit..."
 		break
 	}
-	
+
 	$idx = 0
 	[int]$idxMax = $list.Count - 1
 
@@ -48,13 +60,13 @@ do {
 		For ($i = 0; $i -lt $list.Count; $i++) {
 			if ($i -eq $idx) { Write-Host $list[$i] -ForegroundColor Black -BackgroundColor DarkGreen }
 			else { Write-Host $list[$i] }
-		}
+  }
 
-		Write-Host $header1
-		Write-Host "Press Escape or CTRL+C to exit!" -ForegroundColor Cyan
-		
-		$key = [Console]::ReadKey($true)
-		switch ($key.Key) {
+  Write-Host $header1
+  Write-Host "Press Escape or CTRL+C to exit!" -ForegroundColor Cyan
+
+  $key = [Console]::ReadKey($true)
+  switch ($key.Key) {
 			"UpArrow" {
 				$idx = Get-Index -num $idx -max $idxMax -increment $false
 				break
@@ -80,18 +92,19 @@ do {
 			}
 			Default {
 			}
-		}
-		
-		[Console]::SetCursorPosition(0, 0)
+  }
+
+  [Console]::SetCursorPosition($cursorPos.X, $cursorPos.Y)
 
 	} while ($continue)
 
 } while (!$exit)
+
 # SIG # Begin signature block
 # MIIGMgYJKoZIhvcNAQcCoIIGIzCCBh8CAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQU46YzgdFrLy+nSYFs6ZWqLTzQ
-# DXSgggPIMIIDxDCCAqygAwIBAgIQRqaG/Zc3Po5BF31FOLqbBTANBgkqhkiG9w0B
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUmWLQUZ0FzYfz+4OfG7MCBRgY
+# UvigggPIMIIDxDCCAqygAwIBAgIQRqaG/Zc3Po5BF31FOLqbBTANBgkqhkiG9w0B
 # AQsFADAfMR0wGwYDVQQDDBRNZW55QnVzaCBQcm9kdWN0aW9uczAeFw0yNjA3MDQx
 # MjE4MTZaFw0yOTA3MDQxMjI4MTVaMB8xHTAbBgNVBAMMFE1lbnlCdXNoIFByb2R1
 # Y3Rpb25zMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApsU5F3dB4odF
@@ -114,12 +127,12 @@ do {
 # zq2tXv8sImITL5ULDqcxggHUMIIB0AIBATAzMB8xHTAbBgNVBAMMFE1lbnlCdXNo
 # IFByb2R1Y3Rpb25zAhBGpob9lzc+jkEXfUU4upsFMAkGBSsOAwIaBQCgeDAYBgor
 # BgEEAYI3AgEMMQowCKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEE
-# MBwGCisGAQQBgjcCAQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBS1
-# DfRjZDhMPoCNkWtmNzY7cG+xGjANBgkqhkiG9w0BAQEFAASCAQBuFKgSWdUVeDqj
-# MNWE2E3MpYKwPbzdfM5KU8uINqzjjVIIF0haqVM0mseFVoblXgDq8EVb0JRk3mWz
-# oULjuVHwX/1gaVmcDVOrOxXKFVbyKaed8nzSjiOV18NsbJWX9uAdQs4bS5UVXRvT
-# Moh9dZE9O2fsFGvQGRyIJzeUHVTt33G/FNyyhcVFdun25i/lEOFiC9UrwAb/LunI
-# WYFRv3VACkp2C8XAfb3DD1JGn7AovSEtxaoTvF7ZZq3jT5Pt1tr0huwfCVHc5ETW
-# 4jfJcYNo8+9LuWL4hIIyW+6lJXf8v75/10BV1HNfbKBnNObfCKcZUo/psSttfb14
-# Q6XtvjK7
+# MBwGCisGAQQBgjcCAQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBS+
+# bxW2q4wvro+wHlC8ye9rn5vGCjANBgkqhkiG9w0BAQEFAASCAQAYwrhitlb50DSf
+# Cl4Xv93HnnWWmdgVJAtJXnrvP7YW1SANF/ugcSqIr54UlYqa9cwknxcvkSvWLYLE
+# +vVqVinLvr2VfhuEIldQhhvvzBmjW6BbkpFV1Ew7WHqP0QTIZa18hlnKVDPrwMId
+# s0AemyISX2Ww+4CYP7u+oJk98eXPFcxEXQVb+yiQKbURePQ9uGRhjLlU0zQF46pr
+# 9SF9C962kWvE8Ju604L9Cp17gWubJsQkBav7Ch7tUamu90NUBLL0v9KrYgVK9hs5
+# e0MKlIEt5PHsIShfrzMqpeK0u52m/58wIIi9eI0xDp9rR26sA2hPb+ltLOOdoCty
+# 36EOkGc8
 # SIG # End signature block
